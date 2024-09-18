7410ae0d39c579eb12faf3fd4ad2ae99c557818d
Campbell-E-Soup <cechudley@pstcc.edu>
2024-09-18 09:21:59 -0400

    added program.cs and program folder

---
+++ program/program.cs
@@ -0,0 +1,11 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = "Campbell";
+
+        foreach (char c in name) {
+            Console.WriteLine(c);
+        }
+    }
+}
